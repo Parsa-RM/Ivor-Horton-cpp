d4447ec469719635f5433003f96436bd5eea5078
Parsa-RM <ramezani_pa@yahoo.com>
2024-03-19 17:21:12 +0330

    Upto the end of page 427

--- Chapter_12.cpp
+++ Chapter_12.cpp
@@ -36,4 +36,52 @@ int main() {
 	"CheckingAccount" classes or objects have inherited features from their
 	parent class or object "BankAccount"
 	*/
+	/* -- Polymorphism */
+	/*
+	Useful Youtube links: https://www.youtube.com/watch?v=tIWm3I_Zu7I
+	So basically polymorphism beauty would become visible when it is combined
+	with pointers. As a reminder previously we could only declare pointers to
+	point to known types. For example,
+	*/
+	int *int_ptr; // this could only point to data types of int not double or
+				  // char only int and therefore only functions which accept
+				  // this type could be called with this pointer. However, when
+				  // this becomes different when working with classes and the
+				  // use of inheritance becomes clear and crucial.
+
+	/*
+	when a pointer to a class is created we can use that pointer to point to its
+	child class objects without the need to change the pointer type. The
+	upcoming text has been taken from the book:
+
+	Suppose you have the LoanAccount and CheckingAccount types defined as
+	before, based on the BankAccount type. Suppose further that you have defined
+	objects of these types, debt and cash, respectively, as illustrated in
+	Figure 12-5. Because both types are based on the BankAccount type, a
+	variable of type pointer to BankAccount, such as pAcc in Figure 12-5, can
+	store the address of either of these objects. Figure 12-5 can be useful page
+	426 of the book
+	*/
+	/* - Terminology */
+	/* Nothing useful just a summary */
+	/* - Defining a Class */
+	/*
+	class ClassName {
+		// code that defines the memebr of the class...
+	};
+	You use the public keyword followed by a colon to make all subsequent
+	members accessible from outside the class. Members specified after the
+	private keyword are not accessible from outside the class. public and
+	private are access specifiers for the class members. There’s another access
+	specifier, protected, that you’ll meet later.
+
+	class ClassName {
+	  public:
+		// Code that specifies members that are accessible from outside the
+		// class...
+	  private:
+		// Code that specifies members that are not accessible from outside the
+		// class...
+	}
+	*/
 }
