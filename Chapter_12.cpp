05ded2da6a8aa38088edf0dbcf7479ca5eb97017
Parsa-RM <ramezani_pa@yahoo.com>
2024-03-19 16:39:05 +0330

    Upto the end of Inheritance

---
+++ Chapter_12.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+/* Chapter 8: Defining Your Own Data Type */
+// help:
+
+/*Section is denoted by:
+- <section name>
+*/
+
+/*
+Subsection is denoted by:
+-- <subsection name>
+*/
+
+/*
+Descriptions are commented using multi-line comments.
+Code comments are denited by \\
+*/
+
+int main() {
+	/* - Classes and Object Oriented Programming */
+	/* -- Encapsulation */
+	/*
+	This means that an object's info can only be accessible from the object
+	itself or manipulated by methodes or functions of the object itself
+	*/
+	/* --- Data Hiding */
+	/*
+	This means that an object's data can only be accessible from the object
+	itself or manipulated by methodes or functions of the object itself
+	*/
+	/* -- Inheritance */
+	/*
+	Inheritance is the ability to define one type in terms of another.
+	Figure 12-4 shows this concept perfectly that how "LoanAccount" and
+	"CheckingAccount" classes or objects have inherited features from their
+	parent class or object "BankAccount"
+	*/
+}
