6d9109ed4d015dcb8a6c0c51efb754f9790f61e9
Parsa-RM <ramezani_pa@yahoo.com>
2024-03-24 23:09:40 +0330

    end of implementing copy constructor

--- Chapter_12.cpp
+++ Chapter_12.cpp
@@ -95,12 +95,12 @@ int main() {
 
 	/*if member variables are defined public then you can easily assign a new
 	 * value to them outside the class definition. Like this:*/
-	std::cout << "Before assigning a new value to myBox.MyPublicInt the value
-				 " 			 " of myBox.MyPublicInt is : "
+	std::cout << "Before assigning a new value to myBox.MyPublicInt the value "
+				 "of myBox.MyPublicInt is : "
 			  << myBox.MyPublicInt << std::endl;
 	myBox.MyPublicInt = 12;
-	std::cout << "After assigning a new value to myBox.MyPublicInt the value
-				 " 			 " of myBox.MyPublicInt is : "
+	std::cout << "After assigning a new value to myBox.MyPublicInt the value "
+				 "of myBox.MyPublicInt is : "
 			  << myBox.MyPublicInt << std::endl;
 
 	/* - Constructors */
@@ -139,21 +139,148 @@ int main() {
 	// The second default constructor
 
 	/* -- Defining Functions Outside the Class */
-	/*This subsection only shows how to implement classes definition in a header
-	 * file and implement them in a .cpp file, which we are familiar with.
-	 Note that if your function definition is like this in the header file:
-	 double volume();
-	 then you have to implement it like this(pay attention that the return type
-	 is in the back of scope operators):
+	/*
+	This subsection only shows how to implement class definition in a header
+	file and implement them in a seperate .cpp file, which we are familiar with.
+	Note that if your function definition is like this in the header file:
+	double volume();
+	then you have to implement it like this(pay attention that the return type):
+
 	double Box::volume(){
+	------
+		^
+		|
+		|
+	}*/
+	/* -- Default Arguments for Constructor Parameters */
+	/*
+	Look at the examples in th header file with their corresponding label here
+	*/
+
+	// Default Arguments for Constructor Parameters Version 1
+
+	/*
+	Note that your implementation should not contain any default value
+	anymore if their defaults already exists it in the header file(definition of
+	the function)
+	*/
+
+	/* -- Using a Member Initializer List */
+
+	/*
+	The first useful thing was that the initializer list gets called before the
+	code block below it so, it is more effient. Another reason is that When you
+	initialize a member variable using an assignment statement in the body of
+	the constructor, the member variable is first default initialized (using a
+	constructor call if the variable is an instance of a class), after which the
+	assignment is carried out as a separate operation.
+	Box(.....) : <<initializer List>>
+	{
 		...
 		..
 		.
-	 }*/
-	/* -- Default Arguments for Constructor Parameters */
-	/*Look at the examples in th header file with their corresponding label here
-	 */
-	// Default Arguments for Constructor Parameters Version 1
-	/* Note that your implementation should not contain any default value
-	 * anymore if their defaults already exists it in the header file*/
+	}
+
+	an Example can be found the upcoming line
+	*/
+
+	// Default Arguments for Constructor Parameters Version 2
+
+	/* -- Using the explicit Keyword */
+
+	/*
+	A problem with class constructors with a single parameter is that the
+	compiler can use such a constructor as an implicit conversion from the type
+	of the parameter to the class type. This can produce undesirable results in
+	some circumstances.
+	For example take a look at the constructor of the Cube in the header file
+	*/
+	Cube box1{7.0};
+	Cube box2{3.0};
+	if (box1.hasLargerVolumeThan(box2))
+		std::cout << "box1 is larger than box2." << std::endl;
+	else
+		std::cout << "box2 is larger than or equal to box1." << std::endl;
+
+	std::cout << "volume of box1 is: " << box1.volume() << std::endl;
+	if (box1.hasLargerVolumeThan(50.0))
+		std::cout << "Volume of box1 is greater than 50." << std::endl;
+	else
+		std::cout << "Volume of box1 is less than or equal to 50." << std::endl;
+	/*
+	when the compiler looks at line 206 it sys that:
+		hasLargerVolumeThan() has been called
+		hasLargerVolumeThan() needs a cube object as input
+		but I see an orinary double number
+		Ok then, I convert it to a cube object by myself with 50 set as its side
+	variable shortly it will be seen like this:
+		 box1.hasLargerVolumeThan(Cube{50.0})
+	So the volume of the given Cube object becomes 50 * 50 * 50 which is bigger
+	than 7 * 7 * 7 and the else block gets executed
+	*/
+	/*
+	we can solve this issue by writing the explicit keyword before the Cube
+	constructor like this:
+	explicit Cube(double side) : m_side{side} {};
+	--------
+	   ^
+	   |
+	so the compiler can't implicitly call the constructor of Cube
+	*/
+	/*
+	Tip: By default, you should therefore declare all single-argument
+	constructors as explicit. (And, take care: this includes multi-parameter
+	constructors that are invokable with just a single argument due to default
+	argument values!)
+	*/
+	/* -- Delegating Constructors */
+	/*
+	A class can have several constructors that provide different ways of
+	creating an object. The code for one constructor can call another of the
+	same class in the initialization list. This can avoid repeating the same
+	code in several constructors. Here’s a simple illustration of this using the
+	Box class to create cube boxes:
+	*/
+	// Delegating Constructors
+
+	/* -- The Copy Constructor */
+	/*
+	This constructor only is used for copying on objects data into a new object.
+	Same as the default constructor we have a default copy constructor which
+	will only copy all the values of the member variables of one object to the
+	new constructed object. The point is that sometimes we don't want the
+	default copy constructor (maybe the object contains pointers) in this case
+	we have to implement our own copy constructor.
+	*/
+	/* --- Implementing the Copy Constructor */
+	/*
+	This simple implementation won't work:
+
+	Box::Box(Box box) // Wrong!!
+	: m_length{box.m_length}, m_width{box.m_width}, m_height{box.m_height}
+
+	Because it causes a chain of recursive calls which will finally result in
+	loss of memmory! take a look at the text of the book itself:
+
+	Each member variable of the new object is initialized with the value of the
+	object that is the argument. No code is needed in the body of the copy
+	constructor in this instance. This looks okay, but consider what happens
+	when the constructor is called. The argument is passed by value, but because
+	the argument is a Box object, the compiler arranges to call the copy
+	constructor for the Box class to make a copy of the argument. Of course, the
+	argument to this call of the copy constructor is passed by value, so another
+	call to the copy constructor is required, and so on. In short, you’ve
+	created a situation where an unlimited number of recursive calls to the copy
+	constructor will occur. Your compiler won’t allow this code to compile. To
+	avoid the problem, the parameter for the copy constructor must be a
+	reference. More specifically, it should be a reference-to-const parameter.
+	For the Box class, this looks like this:
+
+					↓
+	Box::Box(const Box& box) : m_length{box.m_length}, m_width{box.m_width},
+	m_height{box.m_height} {}
+
+	Note that a reference-to-const parameter allows const and non-const objects
+	to be copied.
+	*/
 }
--- include/Box.h
+++ include/Box.h
@@ -15,9 +15,36 @@ class Box {
 	/*  Box() {} // The first default constructor */
 	Box() = default; // The second default constructor
 
-	Box(double width = 1.0, double height = 1.0,
+	// Default Arguments for Constructor Parameters Version 1
+
+	/* Box(double width = 1.0, double height = 1.0,
 		double length =
-			1.0); // Default Arguments for Constructor Parameters Version 1
+			1.0);
+	*/
+
+	// Default Arguments for Constructor Parameters Version 2
+	/*
+	Box(double width, double heigth, double length)
+		: m_width{1.0}, m_hieght{1.0}, m_length{1.0} {}
+	*/
+	explicit Box(double side) : Box(side, side, side) {
+		std::cout << "This is a the old Cube using delegating constructors"
+				  << std::endl;
+	}
+	// copy constructor
+	/*
+	You should normally never write this copy constructor of Box yourself. The
+	compiler already generates one just like it by default, so all that could
+	come from defining one yourself are bugs (forgetting to copy one of the
+	member variables) or maintenance issues (forgetting to update the copy
+	constructor if member variables are added). Like we said, this was just to
+	show you how you can define one. We’ll return to the when you should in
+	later chapters. (Spoiler alert: you should do so as little as possible; just
+	let the compiler do all
+	*/
+	Box(const Box &boxToBeCopied)
+		: Box{boxToBeCopied.m_width, boxToBeCopied.m_hieght,
+			  boxToBeCopied.m_length} {};
 	int MyPublicInt{};
 	double volume();
 
@@ -25,5 +52,18 @@ class Box {
 	double m_width;
 	double m_hieght;
 	double m_length;
+};
+
+class Cube {
+  public:
+	Cube(double side) : m_side{side} {}; // Constructor
+	double volume() {
+		return m_side * m_side * m_side;
+	}; // Calculate volume of a cube
+	bool hasLargerVolumeThan(Cube cube) {
+		return volume() > cube.volume();
+	}; // Compare volume of a cube with another
+  private:
+	double m_side;
 };
 #endif
